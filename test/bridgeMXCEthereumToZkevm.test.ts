631a4181d6fbd2f7a6ebbbc6b9f0af286ab1c74a
Mr.Mao <951416545@qq.com>
2024-04-26 09:55:10 +0800

    chore: update code

--- test/bridgeMXCEthereumToZkevm.test.ts
+++ test/bridgeMXCEthereumToZkevm.test.ts
@@ -2,6 +2,7 @@ import {beforeEach, describe, expect, test} from 'vitest'
 import { BigNumber } from "ethers"
 import {bridge2500MXCEthereumToZkevm} from '../src/tasks/bridgeMXCEthereumToZkevm'
 import dayjs from 'dayjs'
+import { formatEther } from 'ethers/lib/utils'
 
 describe('task:bridgeMXCEthereumToZkevm', () => {
   let ethereumTransferMXCRecords: Map<string, BigNumber>
@@ -17,7 +18,7 @@ describe('task:bridgeMXCEthereumToZkevm', () => {
   test('Transfer 50 MXC to ZKEVM from 0x0795...0d7A this week', () => {
     console.log('transfer length: ', [...ethereumTransferMXCRecords.values()].length)
     const address = '0x0795D90c6d60F7c77041862E9aE5059B4d5e0d7A'
-    console.log('transfer amount: ', ethereumTransferMXCRecords.get(address))
+    console.log('transfer amount: ', formatEther(ethereumTransferMXCRecords.get(address) || '0'))
     expect(ethereumTransferMXCRecords.get(address)?.gte(50))
   })
 })
