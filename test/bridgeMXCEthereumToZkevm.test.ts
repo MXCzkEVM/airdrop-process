a4cf0816bdb606f982934c7d7a0f2cab1c4895c8
Mr.Mao <951416545@qq.com>
2024-04-26 12:52:10 +0800

    chore: update code

--- test/bridgeMXCEthereumToZkevm.test.ts
+++ test/bridgeMXCEthereumToZkevm.test.ts
@@ -8,23 +8,7 @@ import { MXCAddressTaskModel, MXCTasksModel } from '../src/models'
 import axios from 'axios'
 
 describe('task:bridgeMXCEthereumToZkevm', () => {
-  let ethereumTransferMXCRecords: Map<string, BigNumber>
-
   beforeEach(async () => {
-    const firstWeekTime = dayjs().day(1).hour(0).minute(0).second(0).unix()
-    ethereumTransferMXCRecords = await bridgeMXCEthereumToZkevm(
-      false,
-      firstWeekTime,
-    )
-  },
-  )
-  test('Transfer 50 MXC to ZKEVM from 0x0795...0d7A this week', () => {
-    // console.log('transfer length: ', [...ethereumTransferMXCRecords.values()].length)
-    const address = '0x0795D90c6d60F7c77041862E9aE5059B4d5e0d7A'
-    // console.log('transfer amount: ', formatEther(ethereumTransferMXCRecords.get(address) || '0'))
-  })
-
-  test('parseDeadlineTasks', async () => {
     const publishedTasks = await getPublishedTasks(dayjs().unix())
 
     // load by github json
