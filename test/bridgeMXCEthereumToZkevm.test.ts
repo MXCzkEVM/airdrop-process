9cca07222ac05795e4d349d0d8663bf64da96cba
Mr.Mao <951416545@qq.com>
2024-04-26 12:50:11 +0800

    chore: update code

--- test/bridgeMXCEthereumToZkevm.test.ts
+++ test/bridgeMXCEthereumToZkevm.test.ts
@@ -1,6 +1,6 @@
-import {beforeEach, describe, expect, test} from 'vitest'
+import { beforeEach, describe, expect, test } from 'vitest'
 import { BigNumber } from "ethers"
-import {bridgeMXCEthereumToZkevm} from '../src/tasks/bridgeMXCEthereumToZkevm'
+import { bridgeMXCEthereumToZkevm } from '../src/tasks/bridgeMXCEthereumToZkevm'
 import dayjs from 'dayjs'
 import { formatEther, parseEther } from 'ethers/lib/utils'
 import { getPublishedTasks, parseTankUID } from '../src/uitls'
@@ -9,16 +9,15 @@ import axios from 'axios'
 
 describe('task:bridgeMXCEthereumToZkevm', () => {
   let ethereumTransferMXCRecords: Map<string, BigNumber>
-    const firstWeekTime = dayjs().day(1).hour(0).minute(0).second(0).unix()
 
   beforeEach(async () => {
+    const firstWeekTime = dayjs().day(1).hour(0).minute(0).second(0).unix()
     ethereumTransferMXCRecords = await bridgeMXCEthereumToZkevm(
+      false,
       firstWeekTime,
-      false
     )
   },
-  0
-)
+  )
   test('Transfer 50 MXC to ZKEVM from 0x0795...0d7A this week', () => {
     // console.log('transfer length: ', [...ethereumTransferMXCRecords.values()].length)
     const address = '0x0795D90c6d60F7c77041862E9aE5059B4d5e0d7A'
@@ -26,7 +25,7 @@ describe('task:bridgeMXCEthereumToZkevm', () => {
   })
 
   test('parseDeadlineTasks', async () => {
-    const publishedTasks = await getPublishedTasks(dayjs().valueOf())
+    const publishedTasks = await getPublishedTasks(dayjs().unix())
 
     // load by github json
     const response = await axios('https://raw.githubusercontent.com/MXCzkEVM/airdrop-tasks/main/tasks.json')
@@ -46,7 +45,7 @@ describe('task:bridgeMXCEthereumToZkevm', () => {
         task_name: item.name,
         task: item.task,
         testnet: item.testnet ? 1 : 0,
-        expiredAt: dayjs().day(6).hour(29).minute(29).second(29).valueOf(),
+        expiredAt: dayjs().day(6).hour(29).minute(29).second(29).unix(),
         zks: item.zks
       }
     })
@@ -60,42 +59,33 @@ describe('task:bridgeMXCEthereumToZkevm', () => {
 
   test('processDeadlineTasks', async () => {
     const publishedTasks = await getPublishedTasks(dayjs().valueOf())
-    const timeByStartWeek = dayjs().day(1).hour(0).minute(0).second(0).unix()
 
     console.log('publishedTasks: ', publishedTasks)
 
     const parseCalls: Record<string, any> = {
-      'mainnet_week-01': async (id: any) => {
-        const ethereumTransferMXCRecords = await bridgeMXCEthereumToZkevm(timeByStartWeek)
+      'mainnet_week-01': async (id: any, s: number, e: number) => {
+        const ethereumTransferMXCRecords = await bridgeMXCEthereumToZkevm(true, s, e)
         for (const address of ethereumTransferMXCRecords.keys()) {
           if (!ethereumTransferMXCRecords.get(address)?.gte(parseEther('2500')))
             continue
-          
           await MXCAddressTaskModel.findOrCreate({ where: { address, task_id: id } })
         }
       },
-      'testnet_week-01': async (id: any) => {
-        const ethereumTransferMXCRecords = await bridgeMXCEthereumToZkevm(timeByStartWeek, false)
-        console.log('ethereumTransferMXCRecords.keys()', [...ethereumTransferMXCRecords.keys()].length)
+      'testnet_week-01': async (id: any, s: number, e: number) => {
+        const ethereumTransferMXCRecords = await bridgeMXCEthereumToZkevm(false, s, e)
         for (const address of ethereumTransferMXCRecords.keys()) {
-          console.log('address: ', address)
-          console.log('value: ', ethereumTransferMXCRecords.get(address)?.toString())
-          console.log('ether: ', parseEther('2500').toString())
-          console.log('adopt: ', ethereumTransferMXCRecords.get(address)?.gte(parseEther('2500')))
           if (!ethereumTransferMXCRecords.get(address)?.gte(parseEther('2500')))
             continue
-
           await MXCAddressTaskModel.findOrCreate({ where: { address, task_id: id } })
         }
       },
-      // 'mainnet_week-02': (id: any) => swap(id, timeByStartWeek),
-      // 'mainnet_week-03': (id: any) => swapWithToSensor1000(id, timeByStartWeek),
-      // 'mainnet_week-04': (id: any) => swapWithToXsd5000(id, timeByStartWeek),
     }
 
     for (const task of publishedTasks) {
-      console.log(`run task --- ${task.id}`, parseTankUID(task))
-      await parseCalls[parseTankUID(task)]?.(task.id)
+      const s = dayjs.unix(task.expiredAt).day(1).hour(0).minute(0).second(0).unix()
+      const e = dayjs.unix(task.expiredAt).day(6).hour(59).minute(59).second(59).unix()
+      console.log('scan time: ', dayjs.unix(task.expiredAt).format())
+      await parseCalls[parseTankUID(task)](task.id, s, e)
     }
   })
 })
